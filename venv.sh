acdf7470b4e10b1974344a6d6e2587a3058ef150
Taisto Gizmo2026 Qvist <taisto.qvist@gmail.com>
2026-02-24 18:30:02 +0100

    venv

--- venv.sh
+++ venv.sh
@@ -1,5 +1,10 @@
-python3 -m venv py3
-source py3/bin/activate
+if [ -d py3 ]
+then
+    source py3/bin/activate	
+else
+    python3 -m venv py3
+    source py3/bin/activate
+fi
 pip3 install -U pip 
 pip3 install -U bleak asyncio Construct paho-mqtt
 
